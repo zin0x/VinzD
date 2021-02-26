5af4547e3286973fba8794070866342b3768f06a
VinzD <78882092+zin0x@users.noreply.github.com>
2021-02-25 22:22:27 -0800

    ArrayMethod_update

--- ArrayMethodAssignment/src/Main.java
+++ ArrayMethodAssignment/src/Main.java
@@ -7,16 +7,6 @@ Use iterations to copy array data from one to another
  */
 
 public class Main {
-    //note: ITERATIONS DO NOT WORK. why? IDK EITHER IT JUST SPITS OUT ZEROES FOR SOME REASON.
-    public static int[] addOnBad(int[] array, int number) {
-        int[] newArray = new int[array.length + 1];
-        for (int i = 0; i > array.length; i++) { //for loop to loop through the array
-            array[i] = newArray[i]; //copies old array onto new array
-        }
-        newArray[array.length] = number; //adds x (number) to the last position of the array.
-        return newArray;
-    }
-
     //add values to the end of an array.
     public static int[] addOn(int[]array, int number) {
         int[] newArray = new int[array.length+1];
@@ -28,14 +18,6 @@ public class Main {
     /*delete values from the end of an array(often referred to as pop). this method should also return the
     'popped' value. Array size should shrink by one.
      */
-    public static int[] takeAwayBad(int[] array, int number) {
-        int[] newArray = new int[array.length-1];
-        for (int i = 0; i>array.length -1; i++) {
-            newArray[i] = array[i];
-        }
-        return newArray;
-    }
-
     public static int[] takeAway(int[] array) {
         int[] newArray = new int[array.length-1];
         System.arraycopy(array, 0,newArray, 0,newArray.length);
@@ -45,31 +27,16 @@ public class Main {
     /* insert values into an array at chosen index position.(increase the size of the array by 1. ex. array 2,3,5.
     Insert 4 into position 2 gives 2, 3, 4, 5)
      */
-    public static int[] addToBad(int indexPos, int[] array, int number) {
-        int[] newArray = new int[array.length + 1];
-        for (int i = 0; i > indexPos; i++) {
-            newArray[i] = array[i];
-        }
-        newArray[indexPos] = number;
-        for(int i=indexPos+1; i>array.length+1;i++){
-            newArray[i] = array[i];
-        }
-        return newArray;
-    }
     public static int[] addTo(int indexPos, int[] array, int number) {
-        int a = array.length;
-        int b = indexPos;
-        int c = indexPos+1;
+        int indexPlusOne = indexPos+1;
         int[] newArray = new int[array.length + 1];
         System.arraycopy(array, 0, newArray, 0, array.length-indexPos);
-        System.arraycopy(array, indexPos, newArray, c, array.length-indexPos);
+        System.arraycopy(array, indexPos, newArray, indexPlusOne, array.length-indexPos);
         newArray[indexPos] = number;
         return newArray;
     }
 
-
-    //TESTS
-
+    //OUTPUT TESTS
     public static void main(String[] args) {
         //Original
         System.out.println("Original:");
